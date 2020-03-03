5d2cbb7c20cef0502b72c8354b2de3f5db6d9b7e
dianaazzam <dianaazzam@DESKTOP-9F18RBI.localdomain>
2020-03-03 03:35:10 +0200

    added chart and tried working on checking bias

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,9 @@
 
 var productsNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
+var clicksArray = [];
+var viewsArray = [];
+
 var images = document.querySelector('#images');
 var firstImage = document.querySelector('#firstImg');
 var secondImage = document.querySelector('#secondImg');
@@ -28,16 +31,33 @@ for (var i = 0; i < productsNames.length; i++) {
 var firstProduct, secondProduct, thirdProduct;
 
 
+
+
+var unBiasedArray1 = [];
+var unBiasedArray2 = [];
+
+
+
 function render() {
+
+
     firstProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
     secondProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
     thirdProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
 
+    unBiasedArray1.push(firstProduct, secondProduct, thirdProduct);
+
+
+
 
     while (firstProduct === secondProduct || secondProduct === thirdProduct || thirdProduct === firstProduct) {
+
         firstProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
         secondProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
         thirdProduct = Product.all[globalRandomNumber(0, Product.all.length - 1)];
+        unBiasedArray1.push(firstProduct, secondProduct, thirdProduct);
+
+
     }
 
 
@@ -52,6 +72,8 @@ function render() {
     thirdImage.setAttribute('alt', thirdProduct.productName);
     thirdImage.setAttribute('src', thirdProduct.imgPath);
     thirdImage.setAttribute('title', thirdProduct.productName);
+
+
 
 }
 
@@ -68,22 +90,65 @@ function productClick(event) {
         if (event.target.id !== 'images') {
             if (event.target.id === 'firstImg') {
                 firstProduct.clicks++;
+                unBiasedArray1.push(firstProduct, secondProduct, thirdProduct);
+
+
             } else if (event.target.id === 'secondImg') {
                 secondProduct.clicks++;
+                unBiasedArray1.push(firstProduct, secondProduct, thirdProduct);
+
+
+
             } else if (event.target.id === 'thirdImg') {
                 thirdProduct.clicks++;
+                unBiasedArray1.push(firstProduct, secondProduct, thirdProduct);
+
+
             }
+
             totalClicks++;
             firstProduct.views++;
             secondProduct.views++;
             thirdProduct.views++;
-            render();
 
         }
+
+        var newArray = [...new Set(unBiasedArray1)];
+        unBiasedArray1 = unBiasedArray1.splice(3);
+
+
+        newArray = unBiasedArray1.slice(0, 3);
+
+        // console.log(unBiasedArray1);
+        // console.log(newArray);
+
+        for (var i = 0; i < 3; i++) {
+            // unBiasedArray1 = unBiasedArray1.splice(3);
+
+
+            while (unBiasedArray1[0] === newArray[i] || unBiasedArray1[1] === newArray[i] || unBiasedArray1 === newArray[i]) {
+                unBiasedArray1 = unBiasedArray1.splice(3)
+
+                render();
+
+
+                console.log(unBiasedArray1);
+                console.log(newArray);
+            }
+        }
+
+
+
+
+
+
     } else {
         images.removeEventListener('click', productClick);
         renderResults();
+        renderChart1();
     }
+
+
 }
 
 function renderResults() {
@@ -115,7 +180,7 @@ function renderResults() {
         tableResult.appendChild(tr2Result);
 
         var td1Result = document.createElement('td');
-        td1Result.textContent = Product.all[i].name;
+        td1Result.textContent = (Product.all[i].name.split('.')[0]);
         tr2Result.appendChild(td1Result);
 
         var td2Result = document.createElement('td');
@@ -126,11 +191,15 @@ function renderResults() {
         td3Result.textContent = Product.all[i].clicks;
         tr2Result.appendChild(td3Result);
 
+        clicksArray.push(Product.all[i].clicks);
+        viewsArray.push(Product.all[i].views);
+
+
+
         // console.log(Product.all[i].name);
 
         // tdResult.textContent = `${Product.all[i].name}: ${Product.all[i].clicks} clicks and ${Product.all[i].views} views.`;
     }
-
 
 
 }
@@ -138,4 +207,39 @@ function renderResults() {
 
 function globalRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function renderChart1() {
+
+    var ctx = document.getElementById('productChart1').getContext('2d');
+    var productChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: productsNames,
+            datasets: [{
+                label: "clicks",
+                backgroundColor: "blue",
+                data: clicksArray
+            }, {
+                label: "views",
+                backgroundColor: "gray",
+                data: viewsArray,
+
+
+            }],
+
+
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
